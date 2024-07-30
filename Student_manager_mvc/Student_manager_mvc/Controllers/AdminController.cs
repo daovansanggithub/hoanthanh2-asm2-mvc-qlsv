c2bcbbde8cf6bc0fccc9393fc29b22df1e035073
daovansanggithub <tialuadangithub@gmail.com>
2024-07-30 18:22:30 +0700

    qlsv2-mvc

---
+++ Student_manager_mvc/Student_manager_mvc/Controllers/AdminController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Student_manager_mvc.Models;
+using System.Diagnostics;
+
+namespace Student_manager_mvc.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+
+        QuanLySinhVienMvcContext db = new QuanLySinhVienMvcContext();
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult manager_student()
+        {
+            // khai báo list , 
+            List<SinhVien> students = new List<SinhVien>();
+            students = db.SinhViens.ToList();
+
+			// Truyền danh sách học sinh tới View để hiển thị
+			return View(students);
+			
+        }
+
+        // quan li tai khoan
+        public IActionResult manager_account()
+        {
+            // khai báo list , 
+            List<TaiKhoan> tk = new List<TaiKhoan>();
+            tk = db.TaiKhoans.ToList();
+
+            // Truyền danh sách học sinh tới View để hiển thị
+            return View(tk);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Student_manager_mvc/Student_manager_mvc/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Student_manager_mvc.Models;
+using System.Diagnostics;
+
+namespace Student_manager_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+		public IActionResult checkpoint()
+		{
+			return View();
+		}
+
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
